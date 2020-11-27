0d5a6fee67ce075dfc8b10cf69d300cef8a284a6
Ferhad Othman <git@ferhado.com>
2020-11-27 02:47:07 +0100

    Update smoothscroll.js

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,11 +1,10 @@
-function smoothScroll(to, duration) {
-  var body = document.body,
-	  start = body.scrollTop,
-	  change = ("#" == to.toString().charAt(0) ? document.getElementById(to.substring(1)).offsetTop : to) - start,
-	  time = 0,
-	  increment = 20;
-  ! function animate() {
-	var r, val = start + change * (r = time += increment, (r /= duration) * r * r);
-	body.scrollTop = val, time < duration && (window.requestAnimationFrame(animate) || window.webkitRequestAnimationFrame(animate) || window.mozRequestAnimationFrame(animate) || window.setTimeout(animate, 1e3 / 60))
-  }()
+function smoothScroll(to, duration, element = window) {
+	var start = element.scrollTop || element.pageYOffset || 0,
+		change = ("#" == to.toString().charAt(0) ? document.getElementById(to.substring(1)).offsetTop : to) - start,
+		time = 0,
+		increment = 20;
+	!function animate() {
+		var r, val = start + change * (r = time += increment, (r /= duration) * r * r);
+		element.scrollTo({ top: val }), time < duration && (window.requestAnimationFrame(animate) || window.webkitRequestAnimationFrame(animate) || window.mozRequestAnimationFrame(animate) || window.setTimeout(animate, 1e3 / 60))
+	}()
 }
